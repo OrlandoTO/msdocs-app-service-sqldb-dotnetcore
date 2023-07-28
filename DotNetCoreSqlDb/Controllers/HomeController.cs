a2fc6d8637ad33954566f39423de885ec9c269fb
OrlandoTO <101068493+OrlandoTO@users.noreply.github.com>
2023-07-27 21:05:50 -0500

    .

--- DotNetCoreSqlDb/Controllers/HomeController.cs
+++ DotNetCoreSqlDb/Controllers/HomeController.cs
@@ -21,7 +21,6 @@ namespace DotNetCoreSqlDb.Controllers
         public IActionResult Privacy()
         {
             return View();
-            
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
